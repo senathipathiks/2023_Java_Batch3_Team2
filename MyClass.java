bb82a9929c57c8904cce7ca250c30354411feb4d
Dharshsun <dharshsun0303@gmail.com>
2023-12-16 14:59:52 +0530

    Dharshsun

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,14 @@
 class MyClass {
-  void add(int x, int y)  //adding two integers 
+  
+ void add(int x, int y)  //adding two integers
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
+ void subtract(int x,int y) //Dharshsun
+ {
+    System.out.println("Sum of two numbers: " &x+y);
+  }
 }
+
+
+
