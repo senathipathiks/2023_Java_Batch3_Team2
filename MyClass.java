5861ab919cc790745f6130d5ed4e66d0e7bed74e
Dharshsun <dharshsun0303@gmail.com>
2023-12-16 14:28:38 +0530

    my java

--- MyClass.java
+++ MyClass.java
@@ -3,4 +3,20 @@ class MyClass {
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
+  
+
+  void sub(int x,int y)
+  {
+          System.out.println("subtraction of two numbers: " &x-y);
+  }
+  void division(int x,int y){
+          System.out.println("division of two nos:" &x/y);
+  }
+  class main{
+          public static void main(String args[]){
+                  add(2,3);
+                  sub(2,3);
+                  division(2,3);
 }
+  }
+
