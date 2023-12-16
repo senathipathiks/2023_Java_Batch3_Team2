e492bec086f8f51a1465a4ba02c1bf0fb93dcaf6
durgesh <durgimurugan501@gmail.com>
2023-12-16 15:11:06 +0530

    Durgesh updated

--- MyClass.java
+++ MyClass.java
@@ -3,12 +3,13 @@ class MyClass {
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
-  void sub(int x, int y) 
+  void sub(int x, int y) //Done by durgesh 
   {
     System.out.println("Subtraction of twpo numbers: "&x-y);
   }
-  void mul(int x, int y){
-	  System.out.println("Multiply two numbers: "&x*y);
+  void mul(int x, int y)// Done by Durgesh
+  {
+    System.out.println("Multiply two numbers: "&x*y);
   }
 
 
